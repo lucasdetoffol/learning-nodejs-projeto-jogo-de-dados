35a44e7ba73e93a16fe496e1b8e757f1917286ef
Your Name <you@example.com>
2019-05-30 11:30:32 -0300

    consertando

--- app/models/jogo.js
+++ app/models/jogo.js
@@ -2,7 +2,7 @@ function Jogo(dados = 5, lados = 6){
 
 	console.log('model: executar constructor de jogo');
 	this.dados = dados;
-	this.lados = dados;
+	this.lados = lados;
 	this.pontos = 0;
 	this.n = 0;
 }
@@ -17,7 +17,10 @@ Jogo.prototype.novoJogo = function(){
 Jogo.prototype.lancarUmDado = function(){
 
 	var valorAleatorio = Math.random();//No intervalo[0,1]]
-	var lado = 1+ Math(valorAleatorio * (this.lados+1));
+   //DE [0,1]*6=>[0,6)
+   //Usando Math.floor temo [0,1,2,3,4,5]
+   //Somando uma unidade temos [1,2,3,4,5]
+   var lado = 1+Math.floor(valorAleatorio * this.lados);
 	return lado;
 }
 
@@ -25,7 +28,7 @@ Jogo.prototype.lancarDados = function(){
 	console.log('model: lancarDados');
 	var lancamento = [];
 	for (var i = 0; i < this.dados; i++){
-		lancamento[i] = this.lancarUmDados();
+		lancamento[i] = this.lancarUmDado();
 	}
    return lancamento;
 }
@@ -34,7 +37,7 @@ Jogo.prototype.contarDados = function(lancamento){
     var contagem = Array(this.lados).fill(0);
     for(var i = 0; i < lancamento.length; i++){
     	var numero = lancamento[i];
-    	contagem[numero] += 1;
+    	contagem[numero-1] += 1;
     }
     return contagem;
 }
@@ -43,4 +46,7 @@ Jogo.prototype.contarDados = function(lancamento){
 module.exports.Jogo = Jogo;
 
 jogo  = new Jogo();
-console.log('lado sorteado: ', jogo.lancarUmDado())
+lancamento = jogo.lancarDados();
+contagem = jogo.contarDados(lancamento);
+console.log('Dados sorteados: ', lancamento);
+console.log('contagem: ', contagem);
